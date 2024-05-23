c0ffecf8df621aac27ae5dcbc2b2dcdc856ef45c
Potafe <bt21me011@nitmz.ac.in>
2024-05-23 14:30:50 +0530

    refactor: app.js

--- app.js
+++ app.js
@@ -111,4 +111,6 @@ app.use(routes);
  */
 
 // Server listens on http://localhost:3000
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log("LISTENING TO " + port);
